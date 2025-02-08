8da7a3cb4e962d3c5d3d7df206289b76589bcdcf
Anastasiya <knesty@yandex.by>
2025-02-08 18:09:20 +0300

    added endpoint

--- hw_1/src/app.ts
+++ hw_1/src/app.ts
@@ -7,7 +7,11 @@ import { testingRouter } from './testing/testing.router';
 export const app = express();
 app.use(express.json());
 app.use(cors());
-
+ 
+app.get('/', (req, res) => {
+    // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
+    res.status(200).json({version: '1.0'})
+})
 
 app.use(SETTINGS.PATH.VIDEOS, videosRouter);
 app.use(SETTINGS.PATH.TESTING, testingRouter);
