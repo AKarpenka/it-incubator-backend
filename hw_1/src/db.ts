c4309d4c382722dc59d789e6ffc9834a886a5e78
Anastasiya <knesty@yandex.by>
2025-02-10 13:15:38 +0300

    fix: commented hardcode

--- hw_1/src/db.ts
+++ hw_1/src/db.ts
@@ -3,18 +3,18 @@ import { Resolutions } from "./videos/constants";
 
 export const db: TDataBase = {
     videos: [
-        {
-            id: 0,
-            title: "string",
-            author: "string",
-            canBeDownloaded: true,
-            minAgeRestriction: null,
-            createdAt: "2025-02-05T20:15:15.190Z",
-            publicationDate: "2025-02-05T20:15:15.190Z",
-            availableResolutions: [
-                Resolutions.P1440
-            ]
-        }
+        // {
+        //     id: 0,
+        //     title: "string",
+        //     author: "string",
+        //     canBeDownloaded: true,
+        //     minAgeRestriction: null,
+        //     createdAt: "2025-02-05T20:15:15.190Z",
+        //     publicationDate: "2025-02-05T20:15:15.190Z",
+        //     availableResolutions: [
+        //         Resolutions.P1440
+        //     ]
+        // }
     ],
 }
 
