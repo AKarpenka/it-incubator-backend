f45222150b1966e85c0f4ebe396193e0ea7bef8c
Anastasiya <knesty@yandex.by>
2025-10-20 16:37:12 +0300

    fix: added projection to repositories

--- hw_2/readme.md
+++ hw_2/readme.md
@@ -4,3 +4,16 @@ to run app:
 npm run watch - перебилживать ts
 npm run dev - обновлять ноду
 npm run dev:watch - npm run watch + npm run dev - основная очка входа
+
+
+**About:**
+**hw_1** - тестовое прилоение на express.js, на get, post, put, delete методы и базу мидлвар. 
+
+**hw_2** - приложение с блогами и постами, где между ними выстроена связь данных через ids. 
+Express.js - get, post, put, delete методы
+MongoDB - cloud. 
+*Endpoints:*
+get, post /posts/
+get, put, delete /posts/:id
+get, post /blogs/
+get, put, delete /blogs/:id
--- hw_2/src/modules/blogs/blogs-repository.ts
+++ hw_2/src/modules/blogs/blogs-repository.ts
@@ -1,17 +1,18 @@
 import { getBlogsCollection } from "../../db/db";
 import { TBlog } from "TDataBase";
 import { v4 as uuid } from "uuid";
+import { WithId } from "mongodb";
 
 export const blogsRepository = {
     getBlogs: async (): Promise<TBlog[]> => {
         const blogsCollection = getBlogsCollection();
 
-        return await blogsCollection.find({}).toArray();
+        return await blogsCollection.find({}, { projection: { _id: 0 } }).toArray();
     },
     getBlogById: async (id: string): Promise<TBlog | null> => {
         const blogsCollection = getBlogsCollection();
 
-        return await blogsCollection.findOne({ id });
+        return await blogsCollection.findOne({ id }, { projection: { _id: 0 } });
     },
     createBlog: async (body: Record<string, string>): Promise<TBlog> => {
         const newBlog: TBlog = {
@@ -27,7 +28,9 @@ export const blogsRepository = {
         const blogsCollection = getBlogsCollection();
         await blogsCollection.insertOne(newBlog);
 
-        return newBlog;
+        const { _id, ...blogWithoutMongoId } = newBlog as WithId<TBlog>;
+
+        return blogWithoutMongoId;
     },
     updateBlogById: async (id: string, body: Record<string, string>): Promise<TBlog | null> => {
         const blogsCollection = getBlogsCollection();
@@ -35,7 +38,7 @@ export const blogsRepository = {
         const updatedBlog = await blogsCollection.findOneAndUpdate(
             { id },
             { $set: { ...body } },
-            { returnDocument: 'after' },
+            { returnDocument: 'after', projection: { _id: 0 } },
         );
 
         return updatedBlog;
