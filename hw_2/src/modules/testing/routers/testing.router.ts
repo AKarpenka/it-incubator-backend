52b7b4c0f9988aef0682d8e3be3b656db99c4580
Anastasiya <knesty@yandex.by>
2025-11-14 15:52:54 +0300

    fix: delete all data for users

--- hw_2/src/modules/testing/routers/testing.router.ts
+++ hw_2/src/modules/testing/routers/testing.router.ts
@@ -1,5 +1,5 @@
 import { HttpStatus } from '../../../core/types/httpStatuses';
-import { blogsCollection, postsCollection } from '../../../db/db';
+import { blogsCollection, postsCollection, usersCollection } from '../../../db/db';
 import { Router, Request, Response } from "express";
 
 export const testingRouter = Router();
@@ -9,6 +9,7 @@ testingRouter.delete('/all-data', async (_req: Request, res: Response) => {
     try {
       await blogsCollection.deleteMany({});
       await postsCollection.deleteMany({});
+      await usersCollection.deleteMany({});
   
       res
         .status(HttpStatus.NoContent)
